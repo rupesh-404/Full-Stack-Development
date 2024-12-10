d53e294708e9e3c6a97fe05fd122656c7b5db098
unknown <ranchyranch49@gmail.com>
2024-12-10 14:02:06 +0545

    Full Stack Development Learning

---
+++ README.md
@@ -0,0 +1,21 @@
+Full Stack Development
+
+What is the Html?
+
+
+what is css?
+
+
+
+
+
+HTML - Structure
+
+CSS - styling
+
+JAVAscript - Functionalities Dynamic client side 
+           - Runs on the engine/ Browser
+
+Bootstrap - FrameWork
+
+Script - Language / Serverside
---
+++ javascript/count.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+function myFunction() {
+   // alert("Hi class");
+    //let myname = prompt("Enter your name: ")
+   // alert("Your name is " + myname)
+    //staticContent = document.getElementById('grey').Value;
+    //staticContent = staticContent + "," + myname;
+    //document.getElementById('grey').val = staticContent;
+    count++;
+    document.getElementById('grey').textContent = `Button clicked ${count} times`;
+}
+
+
+
+   
